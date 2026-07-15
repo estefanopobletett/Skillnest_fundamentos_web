b0272e53c841235f367ee02350764685e14142a6
estefanopobletett <estefanopoblete@liceovvh.cl>
2026-07-15 09:26:29 -0400

    rere

---
+++ STACK_JS/07_arreglos/static/js/script.js
@@ -0,0 +1,15 @@
+// Variables a lista
+
+let nombre1 = "Ana";
+let nombre2 = "Pedro";
+let nombre3 = "María";
+let nombre4 = "Carlos";
+
+//transformar en array y mostrar
+
+let alumnos = ["Ana", "Pedro", "María", "Carlos"];
+
+//Mostrar a María en una alerta
+function ejercicio1(){
+    alert(alumnos[2])
+}
