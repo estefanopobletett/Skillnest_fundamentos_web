ddb5852a8d463599b7522bd678e6f4729da8f296
estefanopobletett <estefanopoblete@liceovvh.cl>
2026-07-27 14:54:37 -0400

    wewewew

---
+++ STACK_JS/10_manos_ala_masa/static/js/script.js
@@ -0,0 +1,50 @@
+// Crea una función llamada pizzaOven que devuelva un objeto con las propiedades:
+
+// corteza: tipo de corteza de la pizza.
+// salsa: tipo de salsa utilizada.
+// quesos: un arreglo con los tipos de queso que tiene la pizza.
+// ingredientes: un arreglo con los ingredientes adicionales.
+// 🍕 Usa la función para crear las siguientes pizzas:
+
+// Una pizza “estilo Chicago” con corteza tradicional, salsa tradicional, mozzarella y los ingredientes pepperoni y salchicha.
+// Una pizza “lanzada a mano” con salsa marinara, mozzarella y feta, y los ingredientes champiñones, aceitunas y cebollas.
+// Crea dos pizzas más con tus ingredientes favoritos.
+// 💻 Muestra en consola los objetos de cada pizza creada.
+
+
+
+function pizzaOven(corteza, salsa, quesos, ingredientes) {
+    let pizza = {};
+    pizza.corteza = corteza;
+    pizza.salsa = salsa;
+    pizza.quesos = quesos;
+    pizza.ingredientes = ingredientes;
+    return pizza;
+}
+
+let pi1 = pizzaOven(
+    "tradicional",
+    "tradicional",
+    "mozarella",
+    ["peperoni", "salchicha", "jamón"],
+);
+let pi2 = pizzaOven(
+    "tradicional",
+    "marinara",
+    "mozarella",
+    ["peperoni", "salchicha", "cebolla"],
+);
+function pizzaxde(){
+alert(`Ingredientes de la primera pizza:
+    corteza: ${pi1.corteza}
+    salsa: ${pi1.salsa}
+    quesos: ${pi1.quesos}
+    ingredientes: ${pi1.ingredientes}`);
+alert(`Ingredientes de la segunda pizza:
+    corteza: ${pi2.corteza}
+    salsa: ${pi2.salsa}
+    quesos: ${pi2.quesos}
+    ingredientes: ${pi2.ingredientes}`);
+
+}
+
