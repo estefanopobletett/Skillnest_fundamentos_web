817092685ff38355ecb4616c9679231de0efe9b6
estefanopobletett <estefanopoblete@liceovvh.cl>
2026-08-03 14:34:22 -0400

    sdsdsd

---
+++ STACK_JS/12_Interactividad_con_click/static/js/script.js
@@ -0,0 +1,32 @@
+document.getElementById("colorButton").addEventListener("click", function () {
+    cambiarColor();
+});
+
+// Función que cambia el color de fondo al hacer clic
+function cambiarColor() {
+    let colores = ["#ff6347", "#66cdaa", "#9370db", "#ffd700", "#4682b4", "#ffa07a"];
+    let colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
+    document.body.style.backgroundColor = colorAleatorio;
+}
+
+document.getElementById("colorButtton").addEventListener("click", function () {
+    cambiarColor();
+});
+
+// Función que cambia el color de fondo al hacer clic
+function cambiarColor() {
+    let colores = ["#ff6347", "#66cdaa", "#9370db", "#ffd700", "#4682b4", "#ffa07a"];
+    let colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
+    document.body.style.backgroundColor = colorAleatorio;
+}
+
+document.getElementById("colorButttton").addEventListener("click", function () {
+    cambiarColor();
+});
+
+// Función que cambia el color de fondo al hacer clic
+function cambiarColor() {
+    let colores = ["#2b444e", "#cd66bf", "#220106", "#0011ff", "#1a18be", "#47c5ff"];
+    let colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
+    document.body.style.backgroundColor = colorAleatorio;
+}
